89f770b42cc86097a6a59a7c0ebed065218d64fc
snakeDling <890531@163.com>
2013-07-05 19:52:37 +0800

    Update question_091 Decode Ways.cpp

--- question_091 Decode Ways.cpp
+++ question_091 Decode Ways.cpp
@@ -6,7 +6,7 @@
         if(s.empty() || s[0] == '0')
             return 0;
         int p = 1, p_1 = 1, p_2 = 1;
-        for(int i=1; i< s.size(); ++i, p_2=p_1, p_1=p){
+        for(int i=1; i<s.size(); ++i, p_2=p_1, p_1=p){
             p = (s[0] == '0') ? 0 : p_1;
             p = (s[i-1] == '1' || (s[i-1] == '2' && s[i] <= '6')) ? p + p_2 : p;
         }
