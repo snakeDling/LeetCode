6ace0d5a674a0ac61866499f8570438ce0ef98ff
snakeling <890531@163.com>
2013-09-08 22:50:38 +0800

    Update question_123 Best Time to Buy and Sell Stock III.cc

--- question_123 Best Time to Buy and Sell Stock III.cc
+++ question_123 Best Time to Buy and Sell Stock III.cc
@@ -7,9 +7,6 @@
         int profit=0, i=1, nlen=prices.size();
         vector<int> dp(m+1,0);
         vector<int> segMax(m+1, 0);
-        for(; i<m && i< nlen; ++i){
-            segMax[i] = dp[i] = max(dp[i-1], dp[i-1]+prices[i]-prices[i-1]);
-        }
         for(; i<nlen; ++i){
             for(int j=m; j>0; --j){
                 dp[j] = max(dp[j], segMax[j-1]) + prices[i] - prices[i-1];
