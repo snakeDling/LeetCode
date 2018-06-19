ecfdfe45e6987930d65150c6c7a4b1d1bf898f71
snakeling <890531@163.com>
2018-06-19 15:16:00 +0800

    Update question_007 Reverse Integer.cc

--- question_007 Reverse Integer.cc
+++ question_007 Reverse Integer.cc
@@ -1,16 +1,16 @@
-    class Solution {  
-    public:  
-        int reverse(int x) {  
-            // Start typing your C/C++ solution below  
-            // DO NOT write int main() function  
+class Solution {
+public:
+    int reverse(int x) {
             bool bNegative = x<0 ? true : false;  
-            x = bNegative ? 0-x : x;  
+            x = bNegative ? 0-x : x; 
+            if (x < 0) return 0;
             int result=0;  
             while(x){  
+                if ((result*10+(x%10))/10 < result) return 0;
                 result = result*10+x%10;  
-                x = x/10;  
+                x = x/10;
             }  
             result = bNegative? 0-result : result;  
             return result;  
-        }  
-    };   
+    }
+};
